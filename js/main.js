161dd64c9173ed52dcd4012a0ecd695d099bbb40
avazbek9999 <avazbe97salimov@yandex.ru>
2021-07-20 21:38:24 +0500

    making structura chenged

--- js/main.js
+++ js/main.js
@@ -4,8 +4,36 @@ var elselect = elForm.querySelector('.form__select')
 var elResultBox = document.querySelector('.result-box')
 
 
-elForm.addEventListener('submit', function (evt) {
-    evt.preventDefault()
+var usd_to_uzb = 10600;
+var ruble_to_uzb = 143;
+var evro_to_uzb = 13000;
 
-    elResultBox.textContent ='Result: ' + elInput.value / elselect.value
+elForm.addEventListener('submit', function (evt) {
+	evt.preventDefault();
+
+	var currensyInput = Number(elInput.value.trim());
+	var currensyselect = (elselect.value);
+
+	
+	if (currensyInput <= 0 || isNaN(currensyInput)) {
+		elInput.style.border = '2px solid red';
+		return;
+	} else {
+		elInput.style.border = '2px solid green';
+	}
+
+
+
+	if (currensyselect === 'dollr') {
+		elResultBox.textContent = 'Resalt: ' + (currensyInput / usd_to_uzb).toFixed(2) + ' USD';
+	} else if (currensyselect === 'rubl') {
+		elResultBox.textContent= 'Resalt: ' + (currensyInput / ruble_to_uzb).toFixed(2) + ' RUBL';
+	} else if (currensyselect === 'evro') {
+		elResultBox.textContent= 'Resalt: ' + (currensyInput / evro_to_uzb).toFixed(2) + ' EVRO';
+	}
+	
+
+
+
+	//elResultBox.textContent = 'Result: ' + elInput.value / elselect.value
 })
